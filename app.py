8920305c52831279e9b689ee475a350a0e0e2dbe
a20688392 <a20688392@gmail.com>
2023-05-13 14:15:55 +0800

    register

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/member', methods=['GET', 'POST'])
+def member():
+    if request.method == 'POST':
+        print("user")
+        print(request.form)
+    return render_template('member.html')
 
 if __name__ == '__main__':
     app.run()
